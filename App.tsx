fff8d8b563ecd1c3448fae43f9815c6aa9043af7
Brenda Lau <brendalaupg@hotmail.com>
2025-06-12 06:54:50 +0800

    chore(app): set local as a constant

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import MapScreen from "./src/screens/MapScreen";
 import enUS from "@ant-design/react-native/lib/locale-provider/en_US";
 import { Provider } from "@ant-design/react-native";
+import { Locale } from "@ant-design/react-native/lib/locale-provider";
+
+const CURRENT_LOCALE: Partial<Locale> = enUS;
 
 const App = () => {
   return (
-    <Provider locale={enUS}>
-      <MapScreen />;
+    <Provider locale={CURRENT_LOCALE}>
+      <MapScreen />
     </Provider>
   );
 };
