889cf9a0a9204837c0c661906b8243897c36fa93
Brenda Lau <brendalaupg@hotmail.com>
2025-06-12 22:40:02 +0800

    task(epic): add search epic and update root epic

--- src/epics/placesEpic.ts
+++ src/epics/placesEpic.ts
@@ -1,9 +1,54 @@
-import { Epic } from "redux-observable";
+import { Epic, ofType } from "redux-observable";
+import { RootState } from "../root";
+import { catchError, map, of, switchMap } from "rxjs";
+import { ajax, AjaxResponse } from "rxjs/ajax";
+import {
+  autocompleteError,
+  autocompleteSuccess,
+  PlacesActions,
+  startAutoComplete,
+} from "../store/reducers/placeSlice";
 
-const GOOGLE_PLACES_API_KEY = process.env.PLACES_API_KEY || "";
-const PLACES_API_BASE_URL = "https://maps.googleapis.com/maps/api/place";
+const PLACES_API_BASE_URL =
+  "https://places.googleapis.com/v1/places:autocomplete";
 
-// Epic for autocomplete search
-export const searchPlacesEpic: Epic = (action$) => action$.pipe();
+export const searchPlacesEpic: Epic<PlacesActions, PlacesActions, RootState> = (
+  action$
+) =>
+  action$.pipe(
+    ofType(startAutoComplete.type),
+    switchMap((action) => {
+      const { input, locationRestriction } = action.payload;
+
+      // Prepare the request body
+      const requestBody: places.AutocompleteRequestPayload = {
+        input,
+        locationRestriction,
+      };
+
+      return ajax<places.AutocompleteResponse>({
+        url: PLACES_API_BASE_URL,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": process.env.PLACES_API_KEY || "",
+        },
+        body: requestBody,
+      }).pipe(
+        map((response: AjaxResponse<places.AutocompleteResponse>) =>
+          autocompleteSuccess(response.response)
+        ),
+        catchError((error: any) =>
+          of(
+            autocompleteError(
+              error?.response?.error?.message ||
+                error?.message ||
+                "Failed to fetch autocomplete suggestions"
+            )
+          )
+        )
+      );
+    })
+  );
 
 export const placesEpics = [searchPlacesEpic];
--- src/root.ts
+++ src/root.ts
@@ -3,13 +3,21 @@ import {
   combineReducers,
   configureStore,
 } from "@reduxjs/toolkit";
-import { placeReducer } from "./store/reducers/placeSlice";
+import { placeReducer, PlacesActions } from "./store/reducers/placeSlice";
 import { combineEpics } from "redux-observable";
 import { placesEpics } from "./epics/placesEpic";
 import { createEpicMiddleware } from "redux-observable";
 
+export interface RootState {
+  place: ReturnType<typeof placeReducer>;
+}
+
 // Middleware
-const epicMiddleware = createEpicMiddleware();
+const epicMiddleware = createEpicMiddleware<
+  PlacesActions,
+  PlacesActions,
+  RootState
+>();
 const rootEpic = combineEpics(...placesEpics);
 
 // Root store
@@ -27,5 +35,4 @@ epicMiddleware.run(rootEpic);
 
 export default rootStore;
 
-export type RootState = ReturnType<typeof rootStore.getState>;
 export type AppDispatch = typeof rootStore.dispatch;
